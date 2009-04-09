d1446ac6a28fc95b51fe5a89d6e4481839ee97ec
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2009-04-08 17:29:38 -0700

    Creating a list view for uploaded files.
    In this particular project I'm keeping the base template for the site in the
    webapp directory.  By default, django looks for templates any directory named
    "templates" within the installed applications, but it does not look for a
    templates directory in the project.  We can add this in the settings.py file.
    
    Application-specific templates are placed in the uploads application.

--- uploads/views.py
+++ uploads/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+from uploads.models import File
 
 def form(request):
     pass
@@ -7,7 +10,11 @@ def get(request, download_key):
     pass
 
 def index(request):
-    return HttpResponse('hello')
+    file_list = File.objects.filter(user=request.user)
+    ctx = {
+        'file_list': file_list,
+        }
+    return render_to_response('uploads/index.html', ctx)
 
 def show(request, id):
     pass
