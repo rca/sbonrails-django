e5d501b2cfa8422d7cbe62ac9c2ade0e76dd47d5
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2009-04-08 17:11:49 -0700

    Adding file model and admin glue.
    The File model will be used to maintain the files that are uploaded by users.
    admin.py contains the code that ties the File model into the admin.

---
+++ uploads/admin.py
@@ -0,0 +1,4 @@
+from uploads.models import File
+from django.contrib import admin
+
+admin.site.register(File)
--- uploads/models.py
+++ uploads/models.py
@@ -1,3 +1,27 @@
+import hashlib
+import time
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class File(models.Model):
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=50)
+    file = models.FileField(upload_to='uploads/%Y/%m/%d')
+    download_key = models.CharField(max_length=40, blank=True, unique=True,
+                                    help_text='leave blank for autofill')
+
+    def __unicode__(self):
+        return self.name
+
+    def save(self):
+        """
+        Set the download key if not given before saving
+        """
+
+        if not self.download_key:
+            sha1 = hashlib.sha1('%s:%s:%s' % \
+                                    (self.user.id, self.name, time.time()))
+            self.download_key = sha1.hexdigest()
+
+        super(File, self).save()
